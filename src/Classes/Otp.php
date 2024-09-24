9ef46860abf6030665714721cf9fc2e66f47bd03
4multimedia <biuro@4multimedia.pl>
2024-09-24 09:33:52 +0200

    1.0.41

--- src/Classes/Otp.php
+++ src/Classes/Otp.php
@@ -115,7 +115,7 @@ class Otp
     /**
      * @throws \Exception
      */
-    public function createToken(Kind $kind, $value, $time = false): void
+    public function createToken(Kind $kind, $value, $time = false): bool|array|null
     {
         $this->kind = $kind;
         if (filter_var($value, FILTER_VALIDATE_EMAIL)) {
